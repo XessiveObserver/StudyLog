0af13ce12b05dc20f8c9bb57e6e7341d06ca296f
Xessive Observer <xessiveobserver@gmail.com>
2021-12-11 16:20:03 +0300

    completion of project skeleton.

--- Suggestion/settings.py
+++ Suggestion/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party apps
-    'StudyLogs'
+    'StudyLogs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_URL = 'users:login'
